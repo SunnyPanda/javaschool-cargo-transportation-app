3027d4981af8a20dbff802582805ad5b5fa4f560
Ekaterina Kozlova <kashtanka.nya@gmail.com>
2018-05-27 21:23:22 +0300

    Disable security for api endpoints

--- src/main/java/com/katekozlova/cargo/config/SecurityConfig.java
+++ src/main/java/com/katekozlova/cargo/config/SecurityConfig.java
@@ -4,12 +4,14 @@ import com.katekozlova.cargo.security.AppUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final AppUserDetailsService appUserDetailsService;
 
     @Autowired
@@ -32,4 +34,8 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder(11);
     }
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/api/**", "/static/**");
+    }
 }
